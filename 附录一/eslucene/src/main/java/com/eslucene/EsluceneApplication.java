cca45d5b6ea7a8ddc16269cddf9650cc10c5bc66
napoay <1965678754@qq.com>
2021-01-20 15:47:57 +0800

    添加附录一

---
+++ 附录一/eslucene/src/main/java/com/eslucene/EsluceneApplication.java
@@ -0,0 +1,13 @@
+package com.eslucene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsluceneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsluceneApplication.class, args);
+	}
+
+}
---
+++ 附录一/eslucene/src/test/java/com/eslucene/EsluceneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eslucene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsluceneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
